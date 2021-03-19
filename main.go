929c63210344fe531dd3957d82b72bf148a34a1d
Stuart Smith <stuartsayswhat@googlemail.com>
2021-03-19 18:41:01 +0000

    updated

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gowebexamples/http-server/api"
+)
 
 func main() {
 	// var animal string
 	// animal = "Lion"
 	// fmt.Println(animal)
-	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
-	http.ListenAndServe(":8081", nil)
+	// http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("hello world"))
+	// })
+	// http.ListenAndServe(":8081", nil)
+
+	srv := api.NewServer()
+	http.ListenAndServe(":8080", srv)
 }
